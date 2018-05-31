5d999275c45922e5063066e66cef42bb91f5cc88
Tyler Sommer <sommertm@gmail.com>
2018-05-31 00:48:04 -0600

    Add some notes

--- go.sh
+++ go.sh
@@ -1,17 +1,12 @@
-# spaaace
-
-Manages static assets served from a DigitalOcean Space.
-
-#### Notes
-
-```bash
+#!/usr/bin/env bash
+set -eo pipefail
 mkdir build || rm -rf build/*
 git clone https://github.com/motki/core build/motki
-zip -9jo motki-schema.zip build/motki/resources/ddl/*
+zip -9jo build/motki-schema.zip build/motki/resources/ddl/*
 # download latest evesde-postgres.dump.bz2 from https://www.fuzzwork.co.uk/dump/
 # download latest Types.zip and Icons.zip from https://developers.eveonline.com/resource/resources
 echo "motki_ddl_zip=https://motki-static.nyc3.digitaloceanspaces.com/evesde/motki-schema.zip
 types_zip=https://motki-static.nyc3.digitaloceanspaces.com/evesde/2018/March2018Release_1.0_Types.zip
 icons_zip=https://motki-static.nyc3.digitaloceanspaces.com/evesde/2018/March2018Release_1.0_Icons.zip
 evesde_bz2=https://motki-static.nyc3.digitaloceanspaces.com/evesde/2018/postgres-schema-2018-05-09.dmp.bz2" > build/current
-```
+
